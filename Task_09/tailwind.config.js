808ba1074ac67c27245fe75ae2b0a1d6666857e0
Sannya Wasim <sannya.wasim01@gmail.com>
2023-04-11 00:38:54 +0500

    Task 09 added

---
+++ Task_09/tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.html"],
+  theme: {
+    extend: {
+      colors: {
+        darkBlue: "#112B3C",
+        lightBlue: "#205375",
+        orange: "#F66B0E",
+        gray: "#EFEFEF",
+      },
+    },
+  },
+  plugins: [],
+};
